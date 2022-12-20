c5a0cf66b44b9146866fdc85b3b4cf6a09863f26
Stefan Kramperger <116301571+skramperger@users.noreply.github.com>
2022-12-20 11:11:19 +0100

    Update Rekursion.c

--- Rekursion.c
+++ Rekursion.c
@@ -21,16 +21,8 @@ int main()
     return 0;
 }
 //---------------------------------------------------------------------------------------------------------------------------------------
-// Labyrinth Finder
+//  Labyrinth Path Finder mit eigenem Feld
 
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 #define SIZE_X 10 
