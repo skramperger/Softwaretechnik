d7fc8a6e8c44a12ebd4080a66f51b880c15c9b30
Stefan Kramperger <116301571+skramperger@users.noreply.github.com>
2023-12-18 21:33:46 +0100

    Update rgb_light_control.cpp
    Creation 18.12.2023

--- 5ABELI/rgb_light_control.cpp
+++ 5ABELI/rgb_light_control.cpp
@@ -1,7 +1,6 @@
 // Task: Joystick that goes up and down, lets you switch between LED1 to LED3 on an ARM board 
 // Pressing the joystick middle down, enables the rgb light depending on the LED Busout value and the poti acts as a dimmer
 
-
 #include "mbed.h"
 
 AnalogIn poti(p19);
@@ -19,7 +18,7 @@ BusOut select(LED1, LED2, LED3);
 
 int main() {
 
-    select = 1;
+    select = 0;
     int old_key_down = 0, old_key_up = 0, old_key_center = 0;
 
     float poti_value = 0.0;     // ranges from 0.0 to 1.0
@@ -30,15 +29,14 @@ int main() {
   while(1) {
     // key up press
     if (old_key_up == 0 && key_up == 1) {
-      if (select == 4) select = 1;
+      if (select == 4 || select == 0) select = 1;
       else select = select << 1;
       wait_us(10000);
     }
 
-
     // key down press
     if (old_key_down == 0 && key_down == 1) {
-      if (select == 1) select = 4;
+      if (select == 1 || select == 0) select = 4;
       else select = select >> 1;
       wait_us(10000);
     }
@@ -55,24 +53,21 @@ int main() {
             light_g.write(1);
             light_b.write(1);
         }
-
         if (select == 2){
             light_g.write(1-poti_value);
             light_r.write(1);
             light_b.write(1);
-        }
-        
+        }        
         if (select == 4){
             light_b.write(1-poti_value);
             light_r.write(1);
             light_g.write(1);
         }
-
     }
     old_key_center = key_center;
     wait_us(10000);
 
-
     }
   }
+
 
